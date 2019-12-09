aa6628fbb1220ef5ac622db57d14bf6813831078
pedrotxt <pedro.goncalves@bandtec.com>
2019-12-09 00:03:59 -0200

    Validando Login

--- projeto-site/public/js/Validação.js
+++
@@ -1,7 +0,0 @@
-function logar() {
-    if (email_txt.value.endsWith('.com') ||  email_txt.value.endsWith('.br')) {
-        erro_email.innerHTML = `Email Válido!`;
-    } else {
-        erro_email.innerHTML = `Email Inválido!`;
-    }
-}
